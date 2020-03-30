df86a0fc07d22ce241ecc5e565e009dcec2d13ab
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-03-29 21:00:31 -0600

    Add sphinx!

--- stoltzmaniac/utils/check_types.py
+++ stoltzmaniac/utils/check_types.py
@@ -1,10 +1,21 @@
 def check_expected_type(actual, expected):
     """
     Checks expected types to ensure data integrity
-    :param actual: any object
-    :param expected: expected data type
-    :return: None (raises exception if types do not match)
+    Parameters
+    ----------
+    actual
+        any object
+
+    expected
+        expected data type
+
+
+    Returns
+    -------
+    None (raises exception if types do not match)
+
     """
+
     if not type(actual) == expected:
         raise TypeError(
             f"Input not of expected type.\n"
--- stoltzmaniac/utils/convert.py
+++ stoltzmaniac/utils/convert.py
@@ -6,8 +6,16 @@ from ..utils.check_types import check_expected_type
 def pd_dataframe_to_ndarray(df: pd.DataFrame):
     """
     Convert a pandas dataframe to a numpy array
-    :param df: pd.DataFrame
-    :return: np.ndarray
+    Parameters
+    ----------
+    df
+        pd.DataFrame
+
+    Returns
+    -------
+    np.ndarray
+
     """
+
     check_expected_type(df, pd.DataFrame)
     return np.array(df)
