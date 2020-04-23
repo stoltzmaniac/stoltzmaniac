6d14df807377069c0016c9b31fc48b17eafc3f20
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-04-23 08:24:15 -0600

    Working copy

--- stoltzmaniac/data_handler/basedata.py
+++ stoltzmaniac/data_handler/basedata.py
@@ -36,7 +36,15 @@ class ArrayData:
 
 
 class TestTrainSplitData:
-    def __init__(self, input_data, train_split: float, seed: int):
+    def __init__(self, input_data: np.ndarray, train_split: float, seed: int):
+        """
+        Splits data into user specified sizes for test and train sets through randomization using np.seed
+        Parameters
+        ----------
+        input_data: np.ndarray
+        train_split: float for percentage of set to be used as training (represented as a decimal 0 < n < 1)
+        seed: int to be used as a random seed for reproducibility
+        """
         self.raw_data = input_data
         self.train_split = train_split
         if not isinstance(seed, int):
@@ -65,46 +73,78 @@ class TestTrainSplitData:
             raise ValueError(
                 f"train_split must be between 0 and 1 (not including), it is {self.train_split}"
             )
+
+        # Find indices for test / train split to use in selection from array
         split_row = round(self.raw_data.shape[0] * self.train_split)
         indices = np.random.permutation(self.raw_data.shape[0])
         train_idx, test_idx = indices[:split_row], indices[split_row:]
 
+        # Ensure that both test and train data sets have more than one row of data
         if len(train_idx) < 1 or len(test_idx) < 1:
             raise ValueError(
                 f"Train / Test break does not result in more than one row in test / train\n"
                 f"len(train_idx) = {len(train_idx)}\n"
                 f"len(test_idx) = {len(test_idx)}"
             )
+
+        # Set select data and use ArrayData type to ensure adherence to standards
         self.train_data = ArrayData(self.raw_data[train_idx, :]).raw_data
         self.test_data = ArrayData(self.raw_data[test_idx, :]).raw_data
 
 
 class ScaleData:
-    def __init__(self, input_data: np.ndarray, scale_type: str = "standardize"):
-        self.raw_data = input_data
+    def __init__(self, input_data: np.ndarray, scale_type: str):
+        """
+        Scales data, may use ONE scale_type: 'normalize', 'standardize', 'min_max', 'scale'
+        Parameters
+        ----------
+        input_data
+        scale_type
+        """
+        self.raw_data = input_data  # used to store raw values for user reference
         self.scale_type = scale_type
         self.x_data = self.raw_data[:, :-1]
         self.y_data = self.raw_data[:, -1]
+
+        # Set statistics to be used in preprocessing - needed during preprocessing for predictions
         self.x_array_mean = np.mean(self.x_data, axis=0)
         self.x_array_max = np.max(self.x_data, axis=0)
         self.x_array_min = np.min(self.x_data, axis=0)
 
+        # A standard deviation may not have a value due to square root, set to none in these cases
         try:
             self.x_array_std = np.std(self.raw_data, axis=0)
         except TypeError as e:
             logging.warning("array_std not possible due to sqrt")
             self.x_array_std = None
 
-    def scale(self, x_data):
-        # TODO: handle for 0 in data (divide by 0)
+        if self.scale_type == "standardized" and self.x_array_std is None:
+            raise ValueError(
+                f"Cannot use scale_type 'standardize' because standard deviation is of None type"
+            )
+
+    def scale(self, x_data: np.ndarray):
+        """
+        Scales data to be used in both training and prediction. Scale should handle for a number of cases
+        This function must be run on ALL data being passed, even if scaling will be of None type
+        Parameters
+        ----------
+        x_data: np.ndarray of ONLY predictor values
+
+        Returns
+        -------
+        np.ndarray of scaled data or returns x_data if self.scale_type is None
+        """
+        # Ensure that scale type is handled by function
         if self.scale_type not in ["normalize", "standardize", "min_max", "scale"]:
             if self.scale_type:
                 raise ValueError(
                     f"scale_type {self.scale_type} not in ['normalize', 'standardize', 'min_max', 'scale']"
                 )
-        if self.raw_data.shape is None:
-            raise ValueError("input_data shape is None")
+        if x_data.shape is None:
+            raise ValueError("x_data shape is None")
 
+        # Star scaling process based off of self.scale_type
         if self.scale_type == "min_max":
             scaled_data = (x_data - self.x_array_min) / (
                 self.x_array_max - self.x_array_mean
@@ -124,20 +164,48 @@ class ScaleData:
 
 class LinearRegression:
     def __init__(self, input_data, train_split=0.7, scale_type=None, seed=123):
-        self.raw_input_data = input_data
+        """
+        Create and train model for linear regression, single or multivariate
+        Parameters
+        ----------
+        input_data: can be of any type easily converted to np.ndarray
+        train_split: float described in TestTrainSplitData model
+        scale_type: str described in ScaleData model
+        seed: flat described in TestTrainSplitData model
+        """
+        self.raw_data = input_data
         self.seed = seed
         self.train_split = train_split
         self.scale_type = scale_type
 
-        self.array_data = ArrayData(self.raw_input_data).raw_data
+        # Set data to ArrayData type in order to ensure it passes requirements
+        self.array_data = ArrayData(self.raw_data).raw_data
+
+        # Split data for test / train
         self.split_data = TestTrainSplitData(
             input_data=self.array_data, train_split=self.train_split, seed=self.seed
         )
+
+        # Set scaling parameters
         self.scaler = ScaleData(self.split_data.train_data, self.scale_type)
+
+        # Scale TRAIN data only
         self.split_data.train_data = self.scaler.scale(self.scaler.x_data)
+
+        # Fit linear regression model
         self.fit()
 
     def preprocess(self, data):
+        """
+        Preprocessing will clean and scale data that is to be used in
+        Parameters
+        ----------
+        data
+
+        Returns
+        -------
+
+        """
         array_data = ArrayData(data).raw_data
         scaled_data = self.scaler.scale(array_data)
         return ArrayData(scaled_data).raw_data
