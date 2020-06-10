7b93aa4f2bbc53006a18aa82decd1b67d4d0139b
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-06-10 17:38:00 -0600

    Uncomment code

--- src/stoltzmaniac/models/supervised/k_nearest_neighbors.py
+++ src/stoltzmaniac/models/supervised/k_nearest_neighbors.py
@@ -71,7 +71,14 @@ class KNearestNeighbors:
         return all_distances
 
 
-# my_array = np.array([[1., 12., 2., 10], [2., 3., 4., 12], [3., 9., 6., 14], [4., 1., 8., 16]])
+# my_array = np.array(
+#     [
+#         [1.0, 12.0, 2.0, 10],
+#         [2.0, 3.0, 4.0, 12],
+#         [3.0, 9.0, 6.0, 14],
+#         [4.0, 1.0, 8.0, 16],
+#     ]
+# )
 # knn = KNearestNeighbors(my_array)
 # data_to_predict = np.array([[1., 12., 2], [2.1, 3.1, 4.1]])
 # data_to_predict = np.array([[1., 12., 2.], [2., 3., 4.], [3., 9., 6.], [4., 1., 8.]])
